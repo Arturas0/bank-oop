d644519b6d21d3af44027475dee7132e0c840fff
Arturas0 <arturas.bnr@gmail.com>
2021-11-30 21:56:47 +0200

    updated db cred

--- bootstrap.php
+++ bootstrap.php
@@ -9,6 +9,6 @@ function getSetting(string $option)
     return $settings[$option] ?? null;
 }
 
-define('URL',  'http://' . $_SERVER["SERVER_NAME"] . '/public/');
+define('URL',  'http://bank-oop.herokuapp.com' . '/public/');
 define('DIR', __DIR__ . '/');
 define('INSTALL_DIR', '/public/');

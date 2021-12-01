eb96f4799af053b6c6a09f89d7e5c9ea5d064513
Arturas0 <arturas.bnr@gmail.com>
2021-12-01 23:14:21 +0200

    updated db cred

--- app/settings.php
+++ app/settings.php
@@ -5,6 +5,6 @@ return [
     'db'   => 'de346v47vppiq2',
     'user' => 'ymuxmrmtauvmpf',
     'pass' => '08d46ee4ce5411ef8f62eef0800bed01081ab0790276ff96f1f8b694bc7e40aa',
-    'dir' => '/',
-    'url' => 'https://bank-oop.herokuapp.com/'
+    'dir' => '/public/',
+    'url' => 'https://bank-oop.herokuapp.com/public/'
 ];
